4dbadf63a69ab70fd235166e20a1371bb54c7fc5
barismar <barisma99@gmail.com>
2021-01-20 22:19:54 +0700

    add .env feature

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/barismar/planner-rest-api/routes"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	loadEnv()
 	router := routes.SetupRouter()
 
-	router.Run(":3000")
+	router.Run(":" + os.Getenv("APP_PORT"))
+}
+
+func loadEnv() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 }
