befbd58614555139100792978615895cf78843f5
barismar <barisma99@gmail.com>
2021-01-26 22:42:03 +0700

    call database initiation

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/barismar/planner-rest-api/app/models"
 	"github.com/barismar/planner-rest-api/config"
 	"github.com/barismar/planner-rest-api/routes"
 	"github.com/joho/godotenv"
@@ -10,6 +11,7 @@ import (
 
 func main() {
 	loadEnv()
+	models.DB = config.InitDatabase()
 	router := routes.SetupRouter()
 
 	router.Run(":" + config.Env("APP_PORT", "3000"))
