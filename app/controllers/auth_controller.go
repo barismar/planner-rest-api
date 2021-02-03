25108944759eba6bafe0fe7915ab7cacab90fde8
barismar <barisma99@gmail.com>
2021-02-03 21:15:28 +0700

    update GetUserInfo to Service

--- app/controllers/auth_controller.go
+++ app/controllers/auth_controller.go
@@ -1,15 +1,12 @@
 package controllers
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 
+	"github.com/barismar/planner-rest-api/app/services"
 	"github.com/barismar/planner-rest-api/config"
 	"github.com/gin-gonic/gin"
-	log "github.com/sirupsen/logrus"
-	"golang.org/x/oauth2"
 )
 
 func Login(context *gin.Context) {
@@ -18,8 +15,7 @@ func Login(context *gin.Context) {
 }
 
 func GoogleCallback(context *gin.Context) {
-	log.SetFormatter(&log.JSONFormatter{})
-	content, err := getUserInfo(
+	content, err := services.GetUserInfo(
 		context.Query("state"),
 		context.Query("code"),
 	)
@@ -32,42 +28,4 @@ func GoogleCallback(context *gin.Context) {
 	context.JSON(http.StatusOK, gin.H{
 		"message": content,
 	})
-}
-
-type Profile struct {
-	Id            string `json:"id"`
-	Email         string `json:"email"`
-	VerifiedEmail bool   `json:"verified_at"`
-	Picture       string `json:"picture"`
-}
-
-func getUserInfo(state string, code string) (Profile, error) {
-	profile1 := Profile{}
-	if state != config.OauthStateString {
-		return profile1, fmt.Errorf("invalid oauth state")
-	}
-
-	token, err := config.GoogleOauthConfig.Exchange(oauth2.NoContext, code)
-	if err != nil {
-		return profile1, fmt.Errorf("code exchange failed: %s", err.Error())
-	}
-
-	response, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?oauth_token=" + token.AccessToken)
-	if err != nil {
-		return profile1, fmt.Errorf("failed getting user info: %s", err.Error())
-	}
-
-	defer response.Body.Close()
-	contents, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return profile1, fmt.Errorf("failed reading response body: %s", err.Error())
-	}
-	jsonErr := json.Unmarshal(contents, &profile1)
-	if jsonErr != nil {
-		log.Fatal(jsonErr)
-	}
-
-	log.Println(profile1)
-
-	return profile1, nil
 }
--- app/services/auth_handler.go
+++ app/services/auth_handler.go
@@ -1,14 +1,49 @@
 package services
 
-import "golang.org/x/oauth2"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
 
-var (
-	GoogleOauthConfig *oauth2.Config
-	oauthStateString  = "random-test"
+	"github.com/barismar/planner-rest-api/config"
+	"golang.org/x/oauth2"
 )
 
-func handleGoogleLogin() string {
-	url := GoogleOauthConfig.AuthCodeURL(oauthStateString)
+type UserInfo struct {
+	Id            string `json:"id"`
+	Email         string `json:"email"`
+	VerifiedEmail bool   `json:"verified_at"`
+	Picture       string `json:"picture"`
+}
 
-	return url
+func GetUserInfo(state string, code string) (UserInfo, error) {
+	userInfo := UserInfo{}
+	if state != config.OauthStateString {
+		return userInfo, fmt.Errorf("invalid oauth state")
+	}
+
+	token, err := config.GoogleOauthConfig.Exchange(oauth2.NoContext, code)
+	if err != nil {
+		return userInfo, fmt.Errorf("code exchange failed: %s", err.Error())
+	}
+
+	response, err := http.Get("https://www.googleapis.com/oauth2/v2/userinfo?oauth_token=" + token.AccessToken)
+	if err != nil {
+		return userInfo, fmt.Errorf("failed getting user info: %s", err.Error())
+	}
+
+	defer response.Body.Close()
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return userInfo, fmt.Errorf("failed reading response body: %s", err.Error())
+	}
+
+	jsonErr := json.Unmarshal(contents, &userInfo)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+
+	return userInfo, nil
 }
