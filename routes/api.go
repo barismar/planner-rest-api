2b40f00a8b23738237924b3a25126243d2b0769e
barismar <barisma99@gmail.com>
2021-02-15 01:12:10 +0700

    Move InitAuth and Add new User routes

--- routes/api.go
+++ routes/api.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"github.com/barismar/planner-rest-api/app/controllers"
+	"github.com/barismar/planner-rest-api/config"
 	"github.com/gin-gonic/gin"
 )
 
@@ -18,9 +19,14 @@ func SetupRouter() *gin.Engine {
 func ApiRoute(route *gin.Engine) {
 	route.GET("/health-check", controllers.HealthCheck)
 
+	route.POST("/users", controllers.StoreUser)
+	route.GET("/users", controllers.GetUsers)
+	route.GET("/users/:id", controllers.ShowUser)
+
 	route.POST("/tasks", controllers.StoreTask)
 	route.GET("/tasks", controllers.GetTasks)
 	route.GET("/tasks/:id", controllers.ShowTask)
+
 }
 
 func TemplateRoute(route *gin.Engine) {
@@ -28,6 +34,7 @@ func TemplateRoute(route *gin.Engine) {
 }
 
 func AuthRoute(route *gin.Engine) {
+	config.InitAuth()
 	route.GET("/login", controllers.Login)
 	route.GET("/google-callback", controllers.GoogleCallback)
 }
